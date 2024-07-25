511a38833aca910102795e6b7da827c562cffb0c
Saienko Volodymyr <48810972+SaenkoVova@users.noreply.github.com>
2024-07-25 21:33:37 +0000

    Add events list rendering function

---
+++ js/global/global.events-list.js
@@ -0,0 +1,158 @@
+const events = [
+  {
+    date: "12 AUG, 12:30 PM",
+    speaker: "Paul Foster",
+    sizes: "(max-width: 1305px) 100vw, 1305px",
+    srcset: [
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_200.png   200w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_356.png   356w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_473.png   473w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_578.png   578w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_671.png   671w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_741.png   741w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_808.png   808w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_888.png   888w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_956.png   956w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1015.png 1015w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1068.png 1068w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1125.png 1125w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1186.png 1186w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1246.png 1246w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1282.png 1282w",
+      "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1305.png 1305w",
+    ],
+    src: "./img/global/events/event-1/event-1_iacc9x_c_scale,w_1305.png",
+    alt: "Event picture",
+    name: "Master-class by Veselin Topalov",
+    link: "#",
+    description:
+      "Save the date to see the game of one of the best chess players of our time. Veselin Topalov will share his experience, show several of his signature moves and techniques, thanks to which he could become a two-time world champion.",
+  },
+  {
+    date: "18 AUG, 5:00 PM",
+    speaker: "Ann King",
+    sizes: "(max-width: 1302px) 100vw, 1302px",
+    srcset: [
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_200.png   200w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_380.png   380w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_520.png   520w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_631.png   631w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_714.png   714w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_792.png   792w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_874.png   874w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_940.png   940w",
+      "./img/global/eveÒnts/event-2/event-2_lucwoa_c_scale,w_1005.png 1005w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1067.png 1067w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1132.png 1132w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1197.png 1197w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1243.png 1243w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1299.png 1299w",
+      "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1302.png 1302w",
+    ],
+    src: "./img/global/events/event-2/event-2_lucwoa_c_scale,w_1302.png",
+    alt: "Event picture",
+    name: "Introduction to the Game of Chess",
+    link: "#",
+    description:
+      "Do you want to learn how to play chess? Then be sure to visit our open master class for beginner chess players. Our tutors will give you the basics of the game of chess. You also can meet our teachers and other members of our club.",
+  },
+  {
+    date: "28 AUG, 2:30 PM",
+    speaker: "Veselin Topalov",
+    sizes: "(max-width: 1272px) 100vw, 1272px",
+    srcset: [
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_200.png   200w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_345.png   345w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_453.png   453w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_557.png   557w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_632.png   632w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_706.png   706w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_767.png   767w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_831.png   831w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_888.png   888w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_946.png   946w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_999.png   999w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1043.png 1043w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1094.png 1094w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1146.png 1146w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1188.png 1188w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1237.png 1237w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1260.png 1260w",
+      "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1272.png 1272w",
+    ],
+    src: "./img/global/events/event-3/event-3_gvfh8l_c_scale,w_1272.png",
+    alt: "Event picture",
+    name: "Master-class by Veselin Topalov",
+    link: "#",
+    description:
+      "Save the date to see the game of one of the best chess players of our time. Veselin Topalov will share his experience, show several of his signature moves and techniques, thanks to which he could become a two-time world champion.",
+  },
+  {
+    date: "28 AUG, 05:00 PM",
+    speaker: "Veselin Topalov",
+    sizes: "(max-width: 1265px) 100vw, 1265px",
+    srcset: [
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_200.png   200w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_366.png   366w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_494.png   494w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_610.png   610w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_699.png   699w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_780.png   780w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_859.png   859w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_935.png   935w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_998.png   998w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1061.png 1061w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1120.png 1120w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1189.png 1189w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1236.png 1236w",
+      "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1265.png 1265w",
+    ],
+    src: "./img/global/events/event-4/event-4_xcto9e_c_scale,w_1265.png",
+    alt: "Event picture",
+    name: "Autograph Session With Veselin Topalov",
+    link: "#",
+    description:
+      "After the master class, you can have a talk with the world chess champion! Veselin Topalov will hold an autograph session, and you will get the opportunity to take a picture with one of the most famous chess players in history.",
+  },
+];
+
+const renderEvents = () => {
+    let eventsHtml = '';
+    for(const event of events) {
+        eventsHtml += `
+            <li class="events__item">
+                <div class="events__item-info">
+                    <p class="events__item-date">${event.date}</p>
+                    <p class="events__item-speaker">${event.speaker}</p>
+                </div>
+                <div class="events__item-image-wrapper">
+                    <img
+                        loading="lazy"
+                        class="events__item-image"
+                        sizes="${event.sizes}"
+                        srcset="${event.srcset.join(',')}"
+                        src="${event.src}"
+                        alt="${event.alt}"
+                    />
+                </div>
+                <div class="events__item-content">
+                    <p class="events__item-title">
+                        <a class="events__item-link" href="${event.link}" target="_blank"
+                        >${event.name}</a
+                        >
+                    </p>
+                    <p class="events__item-description">
+                        ${event.description}
+                    </p>
+                </div>
+            </li>
+        `;
+    }
+
+    setTimeout(() => {
+        document.querySelector('.events__list').innerHTML = eventsHtml;
+    }, 100)
+};
+
+
+renderEvents();
