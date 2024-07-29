f87e06dc2596fb5d848919e4f165dfa754ad7914
AnastasiaRakuta <168209962+AnastasiaRakuta@users.noreply.github.com>
2024-07-29 10:40:37 +0000

    Footer fixes and add js code for FAQ page

---
+++ js/faq.titles.js
@@ -0,0 +1,14 @@
+
+    const headerTitle = document.querySelector(".faq__title");
+    
+    const titles = [
+      'FAQ', 
+      'Frequently Asked Questions', 
+      'Popular Questions', 
+      'Common Questions', 
+      'Most Asked Questions'
+    ];  
+    
+    const getRandomEl = (arr) => arr[ Math.floor(Math.random() * arr.length) ]; 
+    
+    headerTitle.innerText = getRandomEl(titles);
