570fa815e3d9955c24fe0e18e7fdfe1a5edd7ba6
Pavlo <105929381+Korylkevych-Pavlo@users.noreply.github.com>
2024-08-04 21:11:01 +0000

    ADD JS championShip

--- js/results.js
+++ js/results.js
@@ -1,8 +1,5 @@
 function init() {
-    import('./index.about-us-info-cards.js');
-    import('./index.offer-carousel.js');
-    import("./global/global.events-list.js");
-    import('./global/global.championship.js');
+    import('./global/global.championship.js');    
   }
   
   const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
